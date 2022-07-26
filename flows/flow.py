f6ed9fa8ddd6e25d5a399ef4cf04c2d0865e1ac9
Nathan Nowack <thrast36@gmail.com>
2022-07-26 15:41:14 -0500

    update registry url

--- flows/flow.py
+++ flows/flow.py
@@ -4,7 +4,7 @@ from prefect.storage import Docker
 from custom_module import task_a, task_b
 
 storage = Docker(
-    registry_url='zzstoatzz',
+    registry_url='727888219528.dkr.ecr.us-east-2.amazonaws.com',
     dockerfile='Dockerfile'
 )
 
