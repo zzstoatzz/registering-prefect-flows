b0f540e8982f2e83e4e56fd5fdc0cc60cca964c1
Nathan Nowack <thrast36@gmail.com>
2022-07-26 22:56:36 -0500

    try things

--- flows/flow.py
+++ flows/flow.py
@@ -12,6 +12,7 @@ with Flow('sandbox', storage=storage) as flow:
     result = task_a()
     task_b(upstream_tasks=[result])
 
+#super random thing
 
 if __name__ == "__main__":
     flow.run(run_on_schedule=False)
