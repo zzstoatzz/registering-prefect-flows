502d69c27b2210ca0ced9e5cd461bb3bb706373c
Nathan Nowack <thrast36@gmail.com>
2022-07-27 00:11:47 -0500

    add task to flow

--- flows/flow.py
+++ flows/flow.py
@@ -1,16 +1,27 @@
-from prefect import Flow
+from custom_module import task_a, task_b
+from prefect import Flow, task
 from prefect.storage import Docker
 
-from custom_module import task_a, task_b
+import httpx
 
 storage = Docker(
     registry_url='330830921905.dkr.ecr.us-east-1.amazonaws.com',
     dockerfile='Dockerfile'
 )
 
+@task
+def get_cat_fact():
+    data = httpx.get('https://catfact.ninja/fact').json()
+    print(data)
+
 with Flow('sandbox', storage=storage) as flow:
+    
     result = task_a()
     task_b(upstream_tasks=[result])
+    
+    get_cat_fact()
+    
+    
 
 #super random thing
 
